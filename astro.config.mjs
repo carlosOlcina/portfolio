677ef47296673862357e821d18ba1fef6829cf57
carlosOlcina <carlosolcina23@gmail.com>
2026-02-20 21:12:20 +0100

    chore: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  adapter: vercel()
 });
