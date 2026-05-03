9366a41343a3bb39a4f7404c1fb40363e4b6803e
carlosOlcina <carlosolcina23@gmail.com>
2026-05-03 19:12:55 +0200

    feat: create class for hero section texts

---
+++ src/data/heroSectionTexts.ts
@@ -0,0 +1,9 @@
+export default Object.freeze({
+  "title-line-1": "Tu web lista en",
+  "title-line-1-highlight": "días",
+  "title-line-2": "resultados en el primer",
+  "title-line-2-highlight": "clic",
+  subtitle:
+    "Freelance Developer. Apasionado por crear soluciones limpias, eficientes y visualmente atractivas.",
+  "contact-button": "Contáctame",
+});
