249a446bd9ddbac5b4edfd047ddb8ab04e50a91b
carlosOlcina <carlosolcina23@gmail.com>
2026-05-03 12:11:18 +0200

    refactor: create organism for Header

---
+++ src/data/navigationItems.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: "W1TlQGj26vqvRdx_gQDtV",
+    label: "Inicio",
+    href: "#about-me",
+  },
+  {
+    id: "Egxx4A86ATFy5m3mjl-y-",
+    label: "Proyectos",
+    href: "#projects",
+  },
+  {
+    id: "j1HGI3H7EVdWWUCbrf1B4",
+    label: "Contacto",
+    href: "#contact",
+  },
+];
